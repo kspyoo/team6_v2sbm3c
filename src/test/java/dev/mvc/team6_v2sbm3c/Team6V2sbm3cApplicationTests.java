8ecae4c941da588981deedd2b1444d12a52ff161
kspyoo <aosdlwhgdk@naver.com>
2024-05-03 15:15:23 +0900

    ddddd

---
+++ src/test/java/dev/mvc/team6_v2sbm3c/Team6V2sbm3cApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.mvc.team6_v2sbm3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ComponentScan(basePackages = {"dev.mvc"}) 
+class Team6V2sbm3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
