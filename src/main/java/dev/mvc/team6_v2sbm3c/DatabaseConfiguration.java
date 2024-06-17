3b12547c5a4e79dd537ee799043036f0aa50e160
changin00 <kimchangin4556@gmail.com>
2024-06-17 10:20:39 +0900

    .

--- src/main/java/dev/mvc/team6_v2sbm3c/DatabaseConfiguration.java
+++ src/main/java/dev/mvc/team6_v2sbm3c/DatabaseConfiguration.java
@@ -25,10 +25,19 @@ import com.zaxxer.hikari.HikariDataSource;
                            "dev.mvc.memberprofile","dev.mvc.communityattachment",
                            "dev.mvc.memberprofile","dev.mvc.culturefile", "dev.mvc.pet", "dev.mvc.petprofile",
 
+
                            "dev.mvc.facilityreview","dev.mvc.login","dev.mvc.mateapply","dev.mvc.matereview","dev.mvc.reply",// DAO interface를 찾는 위치
 
                            "dev.mvc.facilityreview","dev.mvc.login","dev.mvc.mateapply","dev.mvc.matereview",
-                           "dev.mvc.masterlogin","dev.mvc.notice"}) // DAO interface를 찾는 위치
+                           "dev.mvc.masterlogin","dev.mvc.notice", // DAO interface를 찾는 위치
+
+                           "dev.mvc.memberprofile","dev.mvc.communityattachment", // DAO interface를 찾는 위치
+
+
+                           "dev.mvc.memberprofile","dev.mvc.culturefile",
+                           "dev.mvc.facilityreview","dev.mvc.login","dev.mvc.mateapply"}) // DAO interface를 찾는 위치
+
+
 public class DatabaseConfiguration {
     
     @Autowired
